bc0f1040c3bd617827ebc7024bc9a176c9fd706c
dchuong09 <dchuong09@yahoo.com>
2018-03-19 16:51:30 -0700

    progress

--- functions.js
+++ functions.js
@@ -1,1 +1,12 @@
-// TODO: add your solutions here!
+// Build an ASCII Triangle!
+// toTheNthPwr(2, 3) => 16; 
+
+let toTheNthPwr = (num, pow) => {
+	let product = 1; 
+
+	for (let i = num; i < pow; i++) {
+		product *= i ;
+	}
+
+	return product;
+} 
